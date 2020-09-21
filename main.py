53b55f4bc2b3f67bfa5fd664dbf1dc95486c87ed
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-09-21 17:52:49 -0500

    ahora es una suma

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-def print_hi(name):
-    print(f'Hola, {name}')
-
+def suma():
+    num1 = int(input("Dame un numero:"))
+    num2 = int(input("Dame otro numero:"))
+    print("La suma es: ", num1 + num2)
 
 
 if __name__ == '__main__':
-    print_hi('Miguel')
-
+    suma()
