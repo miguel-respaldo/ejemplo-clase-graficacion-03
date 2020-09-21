055db2ef2e46f6af67448f0f952140a6d9b5eaac
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-09-21 17:54:30 -0500

    ajustando espacios

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def suma():
-    num1 = int(input("Dame un numero:"))
-    num2 = int(input("Dame otro numero:"))
-    print("La suma es: ", num1 + num2)
+    num1 = int(input("Dame un numero: "))
+    num2 = int(input("Dame otro numero: "))
+    print("La suma es:", num1 + num2)
 
 
 if __name__ == '__main__':
