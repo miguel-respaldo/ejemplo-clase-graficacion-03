405b5f1caaf082a635c1ac70f1e5c12fe8d79eac
KaZper_Doble_Cero <31358590+KaZper00@users.noreply.github.com>
2020-09-22 18:42:49 -0500

    Update main.py
    Variable name change :
    num1 to numero1
    num2 to numero2

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def division():
 
 
 def multiplicación():
-    numero1 = int(input("Dame un numero: "))
-    numero2 = int(input("Dame otro numero: "))
-    print("La resta es:", numero1 * numero2)
+    num1 = int(input("Dame un numero: "))
+    num2 = int(input("Dame otro numero: "))
+    print("La resta es:", num1 * num2)
 
 
 
@@ -20,9 +20,9 @@ def resta():
 
 
 def suma():
-    num1 = int(input("Dame un numero: "))
-    num2 = int(input("Dame otro numero: "))
-    print("La suma es:", num1 + num2)
+    numero1 = int(input("Dame un numero: "))
+    numero2 = int(input("Dame otro numero: "))
+    print("La suma es:", numero1 + numero2)
 
 
 
