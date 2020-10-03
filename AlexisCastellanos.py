7b2dfe5e06bf99a705f3a35903c1d61d0854d660
Alexis Gerardo Castellanos Mora <za20011800@zapopan.tecmm.edu.mx>
2020-10-02 22:40:12 -0500

    Mi nombre Alxis Castellanos

---
+++ AlexisCastellanos.py
@@ -0,0 +1,10 @@
+#Instituto Tecnológico José Mario Molina Pasquel y Henriquez Capus Zapopan
+#Alexis Gerardo Castellanos Mora
+#20011800
+#Graficación - Tarea Nombre con .py
+def Mi_Nombre():
+    print("Mi nombre es: Alexis Gerardo Castellanos Mora")
+
+
+if __name__ == '__main__':
+    Mi_Nombre()
