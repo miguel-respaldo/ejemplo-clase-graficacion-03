c0b1650063056ff7b4553cb3faf85e57f5ab0cb7
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-10-19 18:32:48 -0500

    fix lena path

--- histograma-1.py
+++ histograma-1.py
@@ -7,7 +7,7 @@ Taken from : http://acodigo.blogspot.com/2017/08/histogramas-opencv-python.html
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('../resources/lena.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('lena.jpg', cv.IMREAD_GRAYSCALE)
 cv.imshow('Lena',img)
 
 hist = cv.calcHist([img], [0], None, [256], [0, 256])
