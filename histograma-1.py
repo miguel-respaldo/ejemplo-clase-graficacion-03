ba5a80f4bbb6b2ea966c8e2ddb6892be8c4ecd60
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-10-19 18:25:35 -0500

    hacer progrma de histrograma

--- histograma-1.py
+++ histograma-1.py
@@ -1,1 +1,21 @@
-print("Hola Mundo")
+#!/usr/bin/env python
+
+'''
+Taken from : http://acodigo.blogspot.com/2017/08/histogramas-opencv-python.html
+'''
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('../resources/lena.jpg', cv.IMREAD_GRAYSCALE)
+cv.imshow('Lena',img)
+
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+plt.plot(hist, color='gray' )
+
+plt.xlabel('intensidad de iluminacion')
+plt.ylabel('cantidad de pixeles')
+plt.show()
+
+cv.destroyAllWindows()
+
