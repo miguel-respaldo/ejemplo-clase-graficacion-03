bcd24bdd64de9cb73d73f5255dc268f04fd988e3
KaZper_Doble_Cero <31358590+KaZper00@users.noreply.github.com>
2020-10-20 18:51:55 -0500

    Remove Configuration

--- camara1.py
+++ camara1.py
@@ -8,9 +8,9 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) in this line capture video from camarera and show color gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #in this line capture video from camarera and show color gray
 
-    canny = cv2.Canny(frame,100,200)#in this line canny capture video from camarera and show border in color withe
+    #canny = cv2.Canny(frame,100,200)#in this line canny capture video from camarera and show border in color withe
     #Canny code acept parameters : Canny(image, threshold1, threshold2, edges=None, apertureSize=None, L2gradient=None):
 
     # Display the resulting frame
