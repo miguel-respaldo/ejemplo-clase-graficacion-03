009de35372c30f4a8c196583cbe974796e54b5e8
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2020-10-20 17:36:20 -0500

    detección de bordes

---
+++ canny-camera.py
@@ -0,0 +1,20 @@
+
+import cv2
+
+cap = cv2.VideoCapture(2)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    canny = cv2.Canny(frame,100,200)
+
+    # Display the resulting frame
+    cv2.imshow('frame',canny)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
