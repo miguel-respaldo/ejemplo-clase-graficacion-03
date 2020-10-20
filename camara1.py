cd2c20eb222663efab2328100e3773cabbba02b3
Luis Eduardo Velasco Ramos <cpuedy.1024@gmail.com>
2020-10-20 17:52:38 -0500

    I post my name in the file LuisEduardoVelascoRamos.py Update camara1.py

--- camara1.py
+++ camara1.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cv2
 
+#Change Value of 2 to 0
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) in this line capture video from camarera and show color gray
+
+    canny = cv2.Canny(frame,100,200)#in this line canny capture video from camarera and show border in color withe
+    #Canny code acept parameters : Canny(image, threshold1, threshold2, edges=None, apertureSize=None, L2gradient=None):
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame',canny)
+    if cv2.waitKey(1) & 0xFF == ord('q'):#if you press "q" exit of aplication
         break
 
 # When everything done, release the capture
